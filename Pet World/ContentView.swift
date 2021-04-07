e77c03a7f39d13786becf07827beb08e50274715
jenny0714 <jennylin0714@gmail.com>
2021-04-07 12:16:46 +0800

    Project initial

---
+++ Pet World/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pet World
+//
+//  Created by Jenny Lin on 2021/4/5.
+//Image("chicken leg")
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pet World/Pet_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_WorldApp.swift
+//  Pet World
+//
+//  Created by Jenny Lin on 2021/4/5.
+//
+
+import SwiftUI
+
+@main
+struct Pet_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
